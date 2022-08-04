d079a3b0b5ad2b0edea4e52d2215974e7577e354
jquihano <jeremy.quihano@gmail.com>
2022-08-03 17:33:26 -0700

    Setting default value for today's date onload

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
+let url = `https://api.nasa.gov/planetary/apod?api_key=71LrDepajiM2fYdRzs0jbvRcsebUtvKkQuiW76Kz`
+
+
 function getFetch(){
     const choice = document.querySelector('input').value
-    const url = `https://api.nasa.gov/planetary/apod?api_key=71LrDepajiM2fYdRzs0jbvRcsebUtvKkQuiW76Kz&date=${choice}`
+     url = `https://api.nasa.gov/planetary/apod?api_key=71LrDepajiM2fYdRzs0jbvRcsebUtvKkQuiW76Kz&date=${choice}`
     fetch(url)
     //handle the response
     .then(res => res.json())
@@ -17,5 +20,15 @@ function getFetch(){
 })
 }
 
+window.onload = () => {
+    fetch(url).then(r=>r.json()).then(data => 
+    {
+      console.log(data)
+      document.querySelector('img').src = data.hdurl
+        document.querySelector('h2').innerText = data.title
+        document.querySelector('h4').innerText = data.explanation
+      //use data
+     });
+  };
 
 
