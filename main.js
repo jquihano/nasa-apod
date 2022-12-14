bedadf8b9bd6fd71295a393c0fc5a81062d08556
Jeremy Quihano <jeremy.quihano@gmail.com>
2022-12-14 09:43:19 -0800

    getting fetch logic to work

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function getFetch(){
     .then(data => {
         console.log(data)
         document.querySelector('.date').innerText = data.date
-        document.querySelector('img').src = data.hdurl
+        document.querySelector('.nasa-image').src = data.hdurl
         document.querySelector('.title').innerText = data.title
         document.querySelector('h4').innerText = data.explanation
     })
@@ -26,11 +26,60 @@ window.onload = () => {
     {
       console.log(data)
       document.querySelector('.date').innerText = data.date
-      document.querySelector('img').src = data.hdurl
+      document.querySelector('.nasa-image').src = data.hdurl
       document.querySelector('.title').innerText = data.title
       document.querySelector('h4').innerText = data.explanation
       //use data
      });
   };
 
+    document.querySelector('.date').innerText = data.date
+    document.querySelector('.nasa-image').src = data.hdurl
+    document.querySelector('.title').innerText = data.title
+    document.querySelector('h4').innerText = data.explanation
 
+
+  function fetchData() {
+    // Retrieve all data within a 1 week range
+    let endDate = new Date();
+    let startDate = new Date();
+    startDate.setDate(startDate.getDate() - 13)
+    
+    // Convert startDate to YYYY-MM-DD format
+    const yearStart = startDate.getFullYear();
+    const monthStart = String(startDate.getMonth() + 1).padStart(2, "0");
+    const dayStart = String(startDate.getDate()).padStart(2, "0");
+    const startDateFormatted = yearStart + "-" + monthStart + "-" + dayStart;
+  
+    // Convert endDate to YYYY-MM-DD format
+    const yearEnd = endDate.getFullYear();
+    const monthEnd = String(endDate.getMonth() + 1).padStart(2, "0");
+    const dayEnd = String(endDate.getDate()).padStart(2, "0");
+    const endDateFormatted = yearEnd + "-" + monthEnd + "-" + dayEnd;
+  
+    const apiData = fetch(
+      url + `&start_date=${startDateFormatted}&end_date=${endDateFormatted}`
+    )
+    .then((response) => response.json())
+    .then((fetchData) => {
+        
+        for(let i = fetchData.length -1; i > 0 ; i--){
+            const newDiv = document.createElement("div")
+            let word = document.createElement('h1')
+            let para = document.createElement('p')
+            let nasaImg = document.createElement('img')
+            word.innerText = fetchData[i].title
+            para.innerText = fetchData[i].explanation
+            nasaImg.src = fetchData[i].hdurl
+            newDiv.className = 'card'
+            newDiv.appendChild(word)
+            newDiv.appendChild(para)
+            newDiv.appendChild(nasaImg)
+
+            const currentDiv = document.getElementById("div1");
+            currentDiv.appendChild(newDiv)
+        }
+        //fetchData;
+    });
+    return apiData;
+  }
